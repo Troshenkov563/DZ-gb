9427562e5db8f57dc07f8c23717a10a0c466a4ff
Ivan Troshenkov <troshenkov.ii@gmail.com>
2022-08-31 17:01:28 +0300

    123

--- z2.cs
+++ z2.cs
@@ -1,13 +1,1 @@
-﻿Console.WriteLine("Введите число: ");
-int a = int.Parse(Console.ReadLine());
-Console.WriteLine("Введите второе число: ");
-int b = int.Parse(Console.ReadLine());
-
-if(a > b)
-{
-    Console.WriteLine("Число {a} больше чем {b}");
-}
-else if (b > a)
-{
-    Console.WriteLine("Число {a} меньше чем {b}");
-}
+﻿123
