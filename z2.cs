a1f5fb1cda7fe2f6add4d4ff421253dee190e0b5
Ivan Troshenkov <troshenkov.ii@gmail.com>
2022-08-31 16:32:00 +0300

    dz

---
+++ z2.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if(a > b)
+{
+    Console.WriteLine("Число {a} больше чем {b}");
+}
+else if (b > a)
+{
+    Console.WriteLine("Число {a} меньше чем {b}");
+}
